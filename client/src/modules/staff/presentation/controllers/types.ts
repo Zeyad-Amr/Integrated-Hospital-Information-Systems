a7cd8fde8be2e7628383330177c7cb247d43a685
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2023-11-03 19:28:44 +0300

    staff controllers finished

--- client/src/modules/staff/presentation/controllers/types.ts
+++ client/src/modules/staff/presentation/controllers/types.ts
@@ -3,6 +3,7 @@ import StaffEntity from "@/modules/staff/domain/entities/staff-entity";
 // Define the initial state using that type
 export interface StaffState {
     staffList: StaffEntity[];
+    currentStaffMember: StaffEntity | null;
     loading: boolean;
     error: string;
 }
