690c37b1650bd67dafcff9aed73f8f6f03d5fb04
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2023-11-24 23:26:28 +0200

    make visits table

--- client/src/app/dashboard/users/all/page.tsx
+++ client/src/app/dashboard/users/all/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import TestPage from "@/core/shared/components/test";
 import Sidebar from "@/core/layout/sidebar/index";
+import VisitsTableComponent from "@/modules/receptionist/presentation/pages/anomynous-visit";
 
 // ----------------------------------------------------------------------
 
@@ -9,7 +9,7 @@ export default function AllUsers() {
   return (
     <div>
       <Sidebar>
-        <TestPage label={"All Users"} />
+        <VisitsTableComponent />
       </Sidebar>
     </div>
   );
