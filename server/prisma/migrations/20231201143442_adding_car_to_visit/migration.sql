366f673d9f8b4b3d9fa6473ae7f7606c0b8f3912
diaabadr <diaabadr82@gmail.com>
2023-12-04 09:19:07 +0200

    modifying

--- server/prisma/migrations/20231201143442_adding_car_to_visit/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Visit" ADD COLUMN     "attendantID" TEXT,
-ADD COLUMN     "attendantName" TEXT,
-ADD COLUMN     "carId" TEXT;
-
--- AddForeignKey
-ALTER TABLE "Visit" ADD CONSTRAINT "Visit_carId_fkey" FOREIGN KEY ("carId") REFERENCES "CarNumber"("id") ON DELETE SET NULL ON UPDATE CASCADE;
