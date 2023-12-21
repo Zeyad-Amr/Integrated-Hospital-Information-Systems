c2a8df888fa95396ef3fcd6086ba2e9398c91f57
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2023-12-21 23:42:54 +0200

    Create custom full screen dialog

--- client/src/modules/er-area/presentation/components/er-area-form/ErAreaForm.tsx
+++ client/src/modules/er-area/presentation/components/er-area-form/ErAreaForm.tsx
@@ -1,8 +1,20 @@
+import CustomFullScreenDialog from '@/core/shared/components/CustomFullScreenDialog'
+import { Button } from '@mui/material';
 import React from 'react'
 
 const ErAreaForm = () => {
+  const [open, setOpen] = React.useState(false);
+
   return (
-    <div>ErAreaForm</div>
+    <div>
+      <p>ErAreaForm</p>
+      <Button variant="outlined" onClick={() => setOpen(true)}>
+        Open full-screen dialog
+      </Button>
+      <CustomFullScreenDialog open={open} setOpen={setOpen} navTitle='ترياج' >
+        <p>Momen</p>
+      </CustomFullScreenDialog>
+    </div>
   )
 }
 
