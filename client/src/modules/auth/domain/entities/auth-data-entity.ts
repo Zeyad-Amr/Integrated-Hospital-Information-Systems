340921ab26d7138fa66556e68c2b33226f705117
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2024-04-30 16:06:50 +0300

    handle create employee with new PersonalData component

--- client/src/modules/auth/domain/entities/auth-data-entity.ts
+++ client/src/modules/auth/domain/entities/auth-data-entity.ts
@@ -1,42 +1,7 @@
 import AuthInterface from "../interfaces/auth-interface";
 import * as Yup from "yup";
 
-export default class AuthDataEntity implements AuthInterface {
-    private _username: string;
-    private _password?: string;
-    private _email?: string;
-
-    constructor(data: AuthInterface) {
-        this._username = data.username;
-        this._password = data.password;
-        this._email = data.email;
-    }
-
-    //* --------------------- Getters ---------------------
-    get username(): string {
-        return this._username;
-    }
-
-    get password(): string | undefined {
-        return this._password;
-    }
-
-    get email(): string | undefined {
-        return this._email;
-    }
-
-    //* --------------------- Setters ---------------------
-    set username(username: string) {
-        this.username = username;
-    }
-
-    set password(password: string | undefined) {
-        this.password = password;
-    }
-
-    set email(email: string | undefined) {
-        this.email = email;
-    }
+export default class AuthDataEntity {
 
     //* --------------------- Methods ---------------------
     static defaultValue(): AuthInterface {
@@ -50,7 +15,10 @@ export default class AuthDataEntity implements AuthInterface {
     static getSchema(): Yup.ObjectSchema<AuthInterface> {
         return Yup.object().shape({
             username: Yup.string().required('اسم المستخدم مطلوب').min(3, 'اسم المستخدم لا يقل عن 3 حروف').max(45, 'اسم المستخدم لا يزيد عن 45 حرف'),
-            password: Yup.string().min(6, 'الرقم السري لا يقل عن 6 حروف').transform((value, originalValue) => originalValue === undefined ? undefined : value).default(undefined),
+            password: Yup.string().min(6, 'الرقم السري لا يقل عن 6 حروف').matches(
+                /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/,
+                "يجب أن يحتوي على حرف كبير واحد، حرف صغير واحد، رقم واحد"
+            ).transform((value, originalValue) => originalValue === undefined ? undefined : value).default(undefined),
             email: Yup.string().email('البريد الألكتروني غير صحيح').transform((value, originalValue) => originalValue === undefined ? undefined : value).default(undefined),
         }).defined();
     }
