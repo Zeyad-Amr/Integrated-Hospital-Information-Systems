2ffd4e73b6306e9f9fe3034ce5d671d920d462f7
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2024-05-16 02:09:09 +0300

    solve bugs in rooms form and table components + clean their code

--- client/src/modules/subdepartments-crud/presentation/pages/rooms/Rooms.tsx
+++ client/src/modules/subdepartments-crud/presentation/pages/rooms/Rooms.tsx
@@ -1,12 +1,8 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Box } from '@mui/system'
 import RoomsTable from '../../components/rooms/RoomsTable'
-import PrimaryButton from '@/core/shared/components/btns/PrimaryButton'
-import PopUp from '@/core/shared/components/PopUp'
-import RoomsForm from '../../components/rooms/RoomsForm'
 
 const Rooms = () => {
-    const [showDialog, setShawDialog] = useState("none");
 
     return (
         <Box
@@ -16,11 +12,6 @@ const Rooms = () => {
                 margin: "0 auto 0",
             }}
         >
-            <PopUp DialogStateController={setShawDialog} display={showDialog} title="اضــافة غــرقة"
-            >
-                <RoomsForm />
-            </PopUp>
-            <PrimaryButton type='button' title='اضــافة غــرفة' sx={{ marginBottom: "1rem" }} onClick={() => setShawDialog("block")}/>
             <RoomsTable />
         </Box>
     )
