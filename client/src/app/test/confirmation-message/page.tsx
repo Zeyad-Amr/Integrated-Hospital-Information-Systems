5660d883e41fae4ea8bc1a0d439e6845c74f4fec
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2024-05-17 02:10:39 +0300

    create confirmation dialog component + apply it in subdepartments crud module

--- client/src/app/test/confirmation-message/page.tsx
+++ client/src/app/test/confirmation-message/page.tsx
@@ -1,20 +1,25 @@
 "use client";
 
 import ConfirmationDialog from "@/core/shared/components/ConfirmationDialog";
-import LoadingService from "@/core/shared/utils/loading-service";
 import { Button } from "@mui/material";
 import { useState } from "react";
 
-
-const Test = () => {   
-  const [open, setOpen] = useState(false)
+const Test = () => {
+  const [open, setOpen] = useState(false);
+  const testFunction = () => {
+    console.log("testttt");
+  };
   return (
     <div>
       confirmation message
-     <ConfirmationDialog  contentMessage="هل انت متأكد من حذف هذا العنصر؟" open={open} setOpen={setOpen} title="حذف العنصر"  />
-     <Button onClick={() => setOpen(true)}>
-    فتح رساله التاكيد
-     </Button>
+      <ConfirmationDialog
+        confirmFunction={testFunction}
+        contentMessage="هل انت متأكد من حذف هذا العنصر؟"
+        open={open}
+        setOpen={setOpen}
+        title="حذف العنصر"
+      />
+      <Button onClick={() => setOpen(true)}>فتح رساله التاكيد</Button>
     </div>
   );
 };
--- client/src/core/shared/components/ConfirmationDialog.tsx
+++ client/src/core/shared/components/ConfirmationDialog.tsx
@@ -4,7 +4,13 @@ import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
-import { Box, Breakpoint, DialogActions, DialogTitle, Slide } from "@mui/material";
+import {
+  Box,
+  Breakpoint,
+  DialogActions,
+  DialogTitle,
+  Slide,
+} from "@mui/material";
 import { TransitionProps } from "@mui/material/transitions";
 import PrimaryButton from "./btns/PrimaryButton";
 import SecondaryButton from "./btns/SecondaryButton";
@@ -20,7 +26,7 @@ const BootstrapDialog = styled(Dialog)(({ theme }) => ({
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
-  children: React.ReactElement<any, any>;
+    children: React.ReactElement<any, any>;
   },
   ref: React.Ref<unknown>
 ) {
@@ -31,8 +37,9 @@ export default function ConfirmationDialog(props: {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
   maxWidth?: false | Breakpoint;
-  title : string;
+  title: string;
   contentMessage: string;
+  confirmFunction: () => void;
 }) {
   const handleClose = () => {
     props.setOpen(false);
@@ -54,37 +61,23 @@ export default function ConfirmationDialog(props: {
             display: "flex",
             justifyContent: "space-between",
             alignItems: "center",
-            // padding: "0.2rem 0.5rem",
           }}
         >
-            <DialogTitle
-              sx={{ m: 0, p: 2, color: "white", fontWeight: "600" }}
-              id="dialog-title"
-            >
-              {props.title}
-            </DialogTitle>
-          {/* <IconButton
-            aria-label="close"
-            onClick={handleClose}
-            sx={{
-              position: "absolute",
-              right: 8,
-              top: 8,
-              color: "white",
-              margin: "0.5rem 0",
-              fontSize: "2rem",
-              cursor: "pointer",
-            }}
+          <DialogTitle
+            sx={{ m: 0, p: 2, color: "white", fontWeight: "600" }}
+            id="dialog-title"
           >
-            <CloseIcon />
-          </IconButton> */}
+            {props.title}
+          </DialogTitle>
         </Box>
-        <DialogContent  dividers>
-          <Box sx={{fontSize : "1.2rem" ,fontWeight : "500"}} >{props.contentMessage}</Box>
+        <DialogContent dividers>
+          <Box sx={{ fontSize: "1.2rem", fontWeight: "500" }}>
+            {props.contentMessage}
+          </Box>
         </DialogContent>
-        <DialogActions sx={{margin : "0.3rem 0.8rem"}}>
-            <PrimaryButton title="تأكيد" onClick={handleClose} />
-            <SecondaryButton title="الغاء" onClick={handleClose} />
+        <DialogActions sx={{ margin: "0.3rem 0.8rem" }}>
+          <PrimaryButton title="تأكيد" onClick={() => props.confirmFunction()} />
+          <SecondaryButton title="الغاء" onClick={handleClose} />
         </DialogActions>
       </BootstrapDialog>
     </>
