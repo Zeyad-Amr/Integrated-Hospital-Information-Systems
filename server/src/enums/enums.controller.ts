94dbd7eb0b12b24ef00e75298a436b4989abaca5
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2024-05-24 00:59:47 +0300

    update enums

--- server/src/enums/enums.controller.ts
+++ server/src/enums/enums.controller.ts
@@ -10,16 +10,14 @@ import {
 import { EnumsService } from './enums.service';
 import { CreateEnumDto } from './dto/create-enum.dto';
 import { UpdateEnumDto } from './dto/update-enum.dto';
-import { ApiBearerAuth, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Public } from 'src/shared/decorators/public.decorator';
+@ApiTags('enums')
 @ApiBearerAuth()
 @Controller('enums')
 export class EnumsController {
   constructor(private readonly enumsService: EnumsService) { }
 
-  @Post()
-  create(@Body() createEnumDto: CreateEnumDto) {
-    return this.enumsService.create(createEnumDto);
-  }
 
   @Get()
   @ApiOperation({ summary: 'get all enums' })
@@ -28,18 +26,4 @@ export class EnumsController {
     return this.enumsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    return this.enumsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: number, @Body() updateEnumDto: UpdateEnumDto) {
-    return this.enumsService.update(+id, updateEnumDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    return this.enumsService.remove(+id);
-  }
 }
