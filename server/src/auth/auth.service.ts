fc289a630d9d1fe6127d2d93d6d17bce227ad685
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2024-06-05 10:48:19 +0300

    remove password from getme response

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -67,7 +67,7 @@ export class AuthService {
     try {
       const user = await this.authRepo.getByUsername(username);
       const perm = await this.authRepo.getUserPermissions(user.username);
-      delete perm.password;
+      delete user.password;
       return { user: user, permissions: perm.employee.role.Permissions };
     } catch (error) {
       throw error;
