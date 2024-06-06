9e0fae6125416a3ee8fa6d6d5ff13ee917177eb0
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2024-06-06 23:29:57 +0300

    create module structure

---
+++ client/src/modules/examination/models/test-model.ts
@@ -0,0 +1,19 @@
+import { TestInterface } from '../interfaces/test-interface';
+
+export default class TestModel {
+
+    // //* --------------------- Serialization: Convert the model to JSON ---------------------
+    static toJson(entity: TestInterface): any {
+        return {
+            test: entity.test,
+        }
+    }
+
+    // //* --------------------- Deserialization: Create a model from JSON data ---------------------
+    static fromJson(json: any): TestInterface {
+        return {
+            id: json.id,
+            test: json.name,
+        };
+    }
+}
---
+++ client/src/modules/examination/view/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VisitsDashboardPage = () => {
+  return (
+    <div>VisitsDashboardPage</div>
+  )
+}
+
+export default VisitsDashboardPage
