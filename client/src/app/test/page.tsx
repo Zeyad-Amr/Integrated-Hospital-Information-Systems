08d564150df2ddc1a0cbf32a314b0f32e84b5f7b
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2024-06-10 12:26:30 +0300

    remove test file in layers of Emr module

--- client/src/app/test/page.tsx
+++ client/src/app/test/page.tsx
@@ -1,51 +1,10 @@
 "use client";
 
-import { HeaderItem } from "@/core/shared/components/CustomDataTable";
-import ExaminationAccordion from "@/core/shared/components/ExaminationAccordion";
-import { useAppSelector } from "@/core/state/store";
-import AllergiesModel from "@/modules/emr/models/allergies-model";
 import AllergiesComponent from "@/modules/emr/view/components/allergies/AllergiesComponent";
-import AllergiesForm from "@/modules/emr/view/components/allergies/AllergiesForm";
-import RoomsForm from "@/modules/management/presentation/components/rooms/RoomsForm";
-import {
-  deleteRoom,
-  getRoomList,
-} from "@/modules/management/presentation/controllers/thunks/room-thunks";
-import { RoomState } from "@/modules/management/presentation/controllers/types";
-import { useState } from "react";
 
 // ----------------------------------------------------------------------
 
 export default function Test() {
-  const roomsTableHeader: HeaderItem[] = [
-    {
-      id: "name",
-      key: "name",
-      label: "الأســـم",
-      minWidth: 50,
-      maxWidth: 50,
-      tableCellProps: { align: "right" },
-      sortable: true,
-      filterable: false,
-      searchable: true,
-      onClick: () => {},
-    },
-    {
-      id: "location",
-      key: "location",
-      label: "الموقع",
-      minWidth: 50,
-      maxWidth: 50,
-      tableCellProps: { align: "right" },
-      sortable: true,
-      filterable: false,
-      searchable: true,
-      onClick: () => {},
-    },
-  ];
-
-  const roomState: RoomState = useAppSelector((state: any) => state.rooms);
-  const [test, setState] = useState(false);
 
   return <AllergiesComponent />;
 }
