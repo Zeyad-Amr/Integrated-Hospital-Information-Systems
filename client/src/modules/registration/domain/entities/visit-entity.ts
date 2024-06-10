70b18e011d1365d53c8a00eb0a2b31985dbaf22c
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2024-06-10 14:11:02 +0300

    Solve errors

--- client/src/modules/registration/domain/entities/visit-entity.ts
+++ client/src/modules/registration/domain/entities/visit-entity.ts
@@ -55,7 +55,7 @@ export default class VisitEntity {
         return Yup.object().shape({
             kinship: Yup.number()
                 .when(['kinship', 'firstName', 'secondName', 'thirdName', 'fourthName', 'SSN', 'verificationMethod', 'gender', 'birthDate', 'phone', 'governate', 'address'],
-                    (values, schema) => coditionCallback(values, schema, "درجة القرابة مطلوبة", state.lookups.lookups.kinshipTypes.map((e) => e.id))),
+                    (values, schema) => coditionCallback(values, schema, "درجة القرابة مطلوبة", state.lookups.lookups.kinshipTypes.map((e) => +e.id))),
             firstName: Yup.string()
                 .when(['secondName', 'thirdName', 'fourthName', 'SSN', 'verificationMethod', 'gender', 'birthDate', 'phone', 'governate', 'address', 'kinship'],
                     (values, schema) => coditionCallback(values, schema, "الاسم الأول مطلوب"))
@@ -83,10 +83,10 @@ export default class VisitEntity {
                 .matches(/^[0-9]+$/, "يجب أن يكون الرقم القومي رقميًا."),
             verificationMethod: Yup.number()
                 .when(['verificationMethod', 'firstName', 'secondName', 'thirdName', 'fourthName', 'SSN', 'gender', 'birthDate', 'phone', 'governate', 'address', 'kinship'],
-                    (values, schema) => coditionCallback(values, schema, "طريقة التحقق مطلوبة", state.lookups.lookups.identityTypes.map((e) => e.id))),
+                    (values, schema) => coditionCallback(values, schema, "طريقة التحقق مطلوبة", state.lookups.lookups.identityTypes.map((e) => +e.id))),
             gender: Yup.number()
                 .when(['gender', 'firstName', 'secondName', 'thirdName', 'fourthName', 'SSN', 'verificationMethod', 'birthDate', 'phone', 'governate', 'address', 'kinship'],
-                    (values, schema) => coditionCallback(values, schema, "الجنس مطلوب", state.lookups.lookups.genderTypes.map((e) => e.id))),
+                    (values, schema) => coditionCallback(values, schema, "الجنس مطلوب", state.lookups.lookups.genderTypes.map((e) => +e.id))),
             birthDate: Yup.string()
                 .when(['firstName', 'secondName', 'thirdName', 'fourthName', 'SSN', 'verificationMethod', 'gender', 'phone', 'governate', 'address', 'kinship'],
                     (values, schema) => coditionCallback(values, schema, "التاريخ مطلوب")),
@@ -97,7 +97,7 @@ export default class VisitEntity {
                 .matches(/^[0-9]+$/, "يجب أن يكون رقم الهاتف رقميًا."),
             governate: Yup.number()
                 .when(['governate', 'firstName', 'secondName', 'thirdName', 'fourthName', 'SSN', 'verificationMethod', 'gender', 'birthDate', 'phone', 'address', 'kinship'],
-                    (values, schema) => coditionCallback(values, schema, "المحافظة مطلوبة", state.lookups.lookups.governates.map((e) => e.id))),
+                    (values, schema) => coditionCallback(values, schema, "المحافظة مطلوبة", state.lookups.lookups.governates.map((e) => +e.id))),
             address: Yup.string()
                 .when(['firstName', 'secondName', 'thirdName', 'fourthName', 'SSN', 'verificationMethod', 'gender', 'birthDate', 'phone', 'governate', 'kinship'],
                     (values, schema) => coditionCallback(values, schema, "العنوان مطلوب"))
@@ -125,22 +125,22 @@ export default class VisitEntity {
                 .required("يجب ادخال رقم التردد"),
             firstName: Yup.string()
                 .when([],
-                (values, schema) => coditionCallback(values, schema, "الاسم الأول مطلوب"))
+                    (values, schema) => coditionCallback(values, schema, "الاسم الأول مطلوب"))
                 .min(3, "يجب أن يكون الاسم الأول على الأقل 3 أحرف")
                 .max(45, "يجب أن يكون الاسم الأول على الأكثر 45 حرفًا"),
             secondName: Yup.string()
                 .when([],
-                (values, schema) => coditionCallback(values, schema, "الاسم الثاني مطلوب"))
+                    (values, schema) => coditionCallback(values, schema, "الاسم الثاني مطلوب"))
                 .min(3, "يجب أن يكون الاسم الثاني على الأقل 3 أحرف")
                 .max(45, "يجب أن يكون الاسم الثاني على الأكثر 45 حرفًا"),
             thirdName: Yup.string()
                 .when([],
-                (values, schema) => coditionCallback(values, schema, "الاسم الثالث مطلوب"))
+                    (values, schema) => coditionCallback(values, schema, "الاسم الثالث مطلوب"))
                 .min(3, "يجب أن يكون الاسم الثالث على الأقل 3 أحرف")
                 .max(45, "يجب أن يكون الاسم الثالث على الأكثر 45 حرفًا"),
             fourthName: Yup.string()
                 .when([],
-                (values, schema) => coditionCallback(values, schema, "الاسم الرابع مطلوب"))
+                    (values, schema) => coditionCallback(values, schema, "الاسم الرابع مطلوب"))
                 .min(3, "يجب أن يكون الاسم الرابع على الأقل 3 أحرف")
                 .max(45, "يجب أن يكون الاسم الرابع على الأكثر 45 حرفًا"),
             SSN: Yup.string()
@@ -150,8 +150,8 @@ export default class VisitEntity {
                 .matches(/^[0-9]+$/, "يجب أن يكون الرقم القومي رقميًا."),
             verificationMethod: Yup.number()
                 .when(["verificationMethod"],
-                    (values, schema) => coditionCallback(values, schema, "طريقة التحقق مطلوبة", state.lookups.lookups.identityTypes.map((e) => e.id))),
-            gender: Yup.number().required("الجنس مطلوب").oneOf(state.lookups.lookups.genderTypes.map((e) => e.id), "الجنس مطلوب"),
+                    (values, schema) => coditionCallback(values, schema, "طريقة التحقق مطلوبة", state.lookups.lookups.identityTypes.map((e) => +e.id))),
+            gender: Yup.number().required("الجنس مطلوب").oneOf(state.lookups.lookups.genderTypes.map((e) => +e.id), "الجنس مطلوب"),
             birthDate: Yup.string()
                 .when([],
                     (values, schema) => coditionCallback(values, schema, "التاريخ مطلوب")),
@@ -162,7 +162,7 @@ export default class VisitEntity {
                 .matches(/^[0-9]+$/, "يجب أن يكون رقم الهاتف رقميًا."),
             governate: Yup.number()
                 .when(["governate"],
-                    (values, schema) => coditionCallback(values, schema, "المحافظة مطلوبة", state.lookups.lookups.governates.map((e) => e.id))),
+                    (values, schema) => coditionCallback(values, schema, "المحافظة مطلوبة", state.lookups.lookups.governates.map((e) => +e.id))),
             address: Yup.string()
                 .when([],
                     (values, schema) => coditionCallback(values, schema, "العنوان مطلوب"))
@@ -189,7 +189,7 @@ export default class VisitEntity {
                     (values, schema) => {
                         if (isRequired) {
                             if (values[0] === 0) {
-                                return schema.oneOf(state.lookups.lookups.kinshipTypes.map((e) => e.id), "يجب اختيار درجة القرابة").required("يجب اختيار درجة القرابة")
+                                return schema.oneOf(state.lookups.lookups.kinshipTypes.map((e) => +e.id), "يجب اختيار درجة القرابة").required("يجب اختيار درجة القرابة")
                             }
                             return schema.required("يجب اختيار درجة القرابة")
                         }
