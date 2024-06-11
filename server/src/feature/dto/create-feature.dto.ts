46e438d7b204b6230fbcf21814da042fdfa00a52
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2024-06-11 20:46:13 +0300

    refactor: Remove subDepartmentId from CreateFeatureDto Swagger

--- server/src/feature/dto/create-feature.dto.ts
+++ server/src/feature/dto/create-feature.dto.ts
@@ -1,16 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateFeatureDto {
     @ApiProperty({ required: true, example: 'Cardiology test' })
     @IsNotEmpty()
     @IsString()
     name: string;
-
-    @ApiProperty({ required: true, example: 1 })
-    @IsNumber()
-    @IsOptional()
-    subDepartmentId: number;
 
     @ApiProperty({ required: true, example: 'cardiology_test' })
     @IsNotEmpty()
