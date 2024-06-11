acdc2b01cd73b13d06caeee6b4bcbd588bb2eee2
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2024-06-11 21:22:28 +0300

    Enhance margins

--- client/src/modules/examination/view/components/ExaminationHeader.tsx
+++ client/src/modules/examination/view/components/ExaminationHeader.tsx
@@ -1,36 +1,40 @@
-import PrimaryButton from '@/core/shared/components/btns/PrimaryButton'
-import { Typography } from '@mui/material'
-import { Box } from '@mui/system'
-import React from 'react'
+import PrimaryButton from "@/core/shared/components/btns/PrimaryButton";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
 
 interface ExaminationHeaderProps {
-    patientName: string
-    clinicName: string
+  patientName: string;
+  clinicName: string;
 }
 
-const ExaminationHeader = ({ patientName, clinicName }: ExaminationHeaderProps) => {
-    return (
-        <Box sx={{
-            display: 'flex',
-            backgroundColor: 'primary.dark',
-            color: 'white',
-            width: '90%',
-            margin: '0 auto',
-            padding: '0.75rem 1rem',
-            borderTopLeftRadius: '5px',
-            borderTopRightRadius: '5px',
-            justifyContent: 'space-between',
-            alignItems: 'center'
-        }}>
-            <Box sx={{ display: 'flex', gap: '1rem' }}>
+const ExaminationHeader = ({
+  patientName,
+  clinicName,
+}: ExaminationHeaderProps) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        backgroundColor: "primary.dark",
+        color: "white",
+        width: "95%",
+        margin: "0 auto",
+        padding: "0.5rem 1rem",
+        borderTopLeftRadius: "5px",
+        borderTopRightRadius: "5px",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <Box sx={{ display: "flex", gap: "1rem" }}>
+        <Typography> {patientName}</Typography>
+        <Box sx={{ width: "1px", backgroundColor: "#fff" }} />
+        <Typography> {clinicName}</Typography>
+      </Box>
+      <PrimaryButton title="انهاء الزيارة" />
+    </Box>
+  );
+};
 
-                <Typography> {patientName}</Typography>
-                <Box sx={{ width: '1px', backgroundColor: '#fff' }} />
-                <Typography> {clinicName}</Typography>
-            </Box>
-            <PrimaryButton title='انهاء الزيارة' />
-        </Box>
-    )
-}
-
-export default ExaminationHeader
+export default ExaminationHeader;
