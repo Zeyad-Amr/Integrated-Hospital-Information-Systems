7771835c7688677de034795f3ffc81e094d1b323
Abdelrhman Yasser <abdelrhman.abdelhakem00@eng-st.cu.edu.eg>
2024-06-12 10:49:02 +0300

    add complaint

--- client/src/modules/examination/controllers/types.ts
+++ client/src/modules/examination/controllers/types.ts
@@ -1,7 +1,18 @@
 //* Define Types of different initialStates
 
+import { PaginatedList } from "@/core/api";
+import { ComplaintsInterface } from "../interfaces/complaints-interface";
+
 export interface TestState {
     test : string;
     loading: boolean;
     error: string;
 }
+
+export interface ComplaintsState {
+    complaints: PaginatedList<ComplaintsInterface>;
+    currentComplaint: ComplaintsInterface;
+    isFetched: boolean;
+    loading: boolean;
+    error: string;
+  }
--- client/src/modules/examination/view/components/AddComplaint.tsx
+++ client/src/modules/examination/view/components/AddComplaint.tsx
@@ -1,20 +1,44 @@
 import React, { useState } from 'react'
 import { Formik } from "formik";
 import CustomTextField from '@/core/shared/components/CustomTextField';
-import { values } from 'lodash';
 import { Box } from '@mui/system';
 import PrimaryButton from '@/core/shared/components/btns/PrimaryButton';
+import { ExaminationFormComponentPropsInterface } from '@/core/shared/components/ExaminationAccordion';
+import { updateComplaint } from '../../controllers/thunks/complaints-thunk';
+import { ComplaintsInterface } from '../../interfaces/complaints-interface';
+import ComplaintsModel from '../../models/complaints-model';
+import { useAppDispatch } from '@/core/state/store';
 
-const AddComplaint = () => {
-
+const AddComplaint = ({
+    visitCode,
+    initialValues,
+     }: ExaminationFormComponentPropsInterface) => {
     const [editing, setEditing] = useState<boolean>(false)
-
+    const dispatch = useAppDispatch();
     return (
         <Formik
-            initialValues={{ complaint: '' }}
-            onSubmit={(values) => {
-                console.log(values)
+            initialValues={
+                initialValues
+                    ? ({
+                        ...initialValues,
+
+                    } as ComplaintsInterface)
+                    : ComplaintsModel.defaultValues()
+            }
+            onSubmit={async (values) => {
+                const submitObject = {
+                    ...values,
+                    visitCode: visitCode,
+                };
+
+                const action = updateComplaint(submitObject);
+
+                dispatch(action).then((res) => {
+                    if (res?.meta.requestStatus == "fulfilled") {
+                    }
+                });
             }}
+            validationSchema={ComplaintsModel.complaintsFormValidations()}
         >
             {({
                 values,
@@ -27,15 +51,15 @@ const AddComplaint = () => {
                 <Box component="form" onSubmit={handleSubmit} noValidate>
                     <CustomTextField
                         isRequired
-                        name="complaint"
+                        name="mainComplaint"
                         label="الشكوى"
                         multiline
-                        rows={7}
-                        value={values.complaint}
+                        rows={4}
+                        value={values.mainComplaint}
                         onChange={handleChange}
                         onBlur={handleBlur}
-                        error={errors.complaint}
-                        touched={touched.complaint}
+                        error={errors.mainComplaint}
+                        touched={touched.mainComplaint}
                         width="100%"
                         props={{
                             type: "text",
