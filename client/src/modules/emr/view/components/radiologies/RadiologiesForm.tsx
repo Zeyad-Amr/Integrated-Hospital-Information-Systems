5da72476fbb54dfaa1576126a751e917da74d96a
Abdelrhman Yasser <abdelrhman.abdelhakem00@eng-st.cu.edu.eg>
2024-06-12 03:55:22 +0300

    return visitCode & patientID

--- client/src/modules/emr/view/components/radiologies/RadiologiesForm.tsx
+++ client/src/modules/emr/view/components/radiologies/RadiologiesForm.tsx
@@ -34,8 +34,8 @@ const RadiologiesForm = ({
       onSubmit={async (values) => {
         const submitObject = {
           ...values,
-          patientId: "3c32c0ff-c15e-4d1a-9caf-1bd7c03f839b",
-          visitCode: "202406102",
+          patientId: patientId,
+          visitCode: visitCode,
         };
 
         const action = initialValues
