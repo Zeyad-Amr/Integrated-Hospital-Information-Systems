cb0e23d90fea3c58d316063cf699b1cf85372622
Mo'men Mohamed <84360276+momen882001@users.noreply.github.com>
2024-06-12 12:56:50 +0300

    remove warnings

--- client/src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import RadiologiesComponent from "@/modules/emr/view/components/radiologies/RadiologiesComponent";
-
-// ----------------------------------------------------------------------
-
-export default function Test() {
-  return <RadiologiesComponent />;
-}
