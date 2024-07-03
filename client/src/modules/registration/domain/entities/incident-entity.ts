4eed15e7ef8bc416433c8ea4fda00bc8efafe692
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2024-07-03 22:49:15 +0300

    fix typo in incident entity

--- client/src/modules/registration/domain/entities/incident-entity.ts
+++ client/src/modules/registration/domain/entities/incident-entity.ts
@@ -5,7 +5,7 @@ export default class IncidentEntity {
 
     static defaultValue(): IncidentInterface {
         return {
-            numberOfPatients : "",
+            numOfPatients : "",
             numberOfIncompletedVisits: "",
             additionalInfo: undefined,
             companions: undefined,
