a023f7af0376a5d1be65e9eed01aeef2def01c26
Ahmed Abd El-Raouf <araouf481@hotmail.com>
2024-07-05 17:32:44 +0300

    fix gender bug

--- client/src/modules/registration/domain/entities/visit-entity.ts
+++ client/src/modules/registration/domain/entities/visit-entity.ts
@@ -160,7 +160,7 @@ export default class VisitEntity {
             verificationMethod: Yup.number()
                 .when(["verificationMethod"],
                     (values, schema) => coditionCallback(values, schema, "طريقة التحقق مطلوبة", state.lookups.lookups.identityTypes.map((e) => +e.id))),
-            gender: Yup.number().required("الجنس مطلوب"),
+            gender: Yup.number().required("الجنس مطلوب").oneOf([1,2], "الجنس مطلوب"),
             birthDate: Yup.string()
                 .when([],
                     (values, schema) => coditionCallback(values, schema, "التاريخ مطلوب")),
